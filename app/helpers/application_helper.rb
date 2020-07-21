7a698d0c903e7a77254b2010247691463ecc4376
Andreas <sgandreas@csd.auth.gr>
2020-07-21 18:58:24 +0300

    Added new recipe creation with tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def avatar_image(user, options = {size: 80})
+		size = options[:size]
+		img_url = "https://image.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg"
+		image_tag(img_url, alt: user.chefname, class: "img-circle user-image")
+	end
+
 end
